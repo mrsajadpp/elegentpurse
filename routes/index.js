438cd057ee40152c92d3f00ac8852cec96175e71
mishalmk <mishalmk115@gmail.com>
2024-03-12 12:34:38 +0530

    second

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,9 @@
 var express = require('express');
-var router = express.Router();
+let db = require('../db/config');
+
+db(process.env.MONGO_STRING, 'elegentpurse')
+
+var router = express.Router(); 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {

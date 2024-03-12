ddbf029cdefbf29d73ea44882ee7eb18d5c893a0
mishalmk <mishalmk115@gmail.com>
2024-03-12 12:23:13 +0530

    first commit

---
+++ db/config.js
@@ -0,0 +1,17 @@
+const { MongoClient } = require('mongodb');
+
+async function connectToMongoDB(url, dbName) {
+    try {
+        const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
+        await client.connect();
+        console.log('Connected to MongoDB server');
+
+        const db = client.db(dbName);
+        return db;
+    } catch (error) {
+        console.error('Error connecting to MongoDB:', error);
+        throw error;
+    }
+}
+
+module.exports = connectToMongoDB;

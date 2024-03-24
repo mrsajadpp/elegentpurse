408490df901c5eb5f0e613143d7d619d91afe020
mishalmk <mishalmk115@gmail.com>
2024-03-24 09:52:04 +0530

    otp bug fixed

--- routes/user.js
+++ routes/user.js
@@ -11,38 +11,61 @@ router.get('/', function (req, res, next) {
 router.post('/auth/signup', async function (req, res, next) {
   try {
     const userCollection = db.get().collection('USER');
-    const userExist = userCollection.findOne({ email: req.body.email, status: true });
-
+    const userExist = await userCollection.findOne({ email: req.body.email, status: true });
+    const userExistNotVerified = await userCollection.findOne({ email: req.body.email, status: false });
     if (!userExist) {
-      // Generate 6-digit OTP
-      const otp = Math.floor(100000 + Math.random() * 900000);
-      const hashedPassword = crypash.hash('sha256', req.body.password);
+      if (userExistNotVerified) {
+        // Generate 6-digit OTP
+        let otp = Math.floor(100000 + Math.random() * 900000);
+        const hashedPassword = crypash.hash('sha256', req.body.password);
 
-      // Check if OTP expired
-      if (userExist && userExist.otp_expiry && userExist.otp_expiry > new Date()) {
-        otp = Math.floor(100000 + Math.random() * 900000); // Generate new OTP
+        // Check if OTP expired
+        if (userExistNotVerified && userExistNotVerified.otp_expiry && userExistNotVerified.otp_expiry > new Date()) {
+          otp = Math.floor(100000 + Math.random() * 900000); // Generate new OTP
+        }
+
+        if (userExistNotVerified.otp_expiry > new Date()) {
+          otp = userExistNotVerified.otp;
+        }
+
+        // Create new user document
+        const newUser = {
+          first_name: req.body.first_name,
+          last_name: req.body.last_name,
+          email: req.body.email,
+          phone: req.body.phone,
+          password: hashedPassword,
+          status: false,
+          otp: otp,
+          otp_expiry: new Date(Date.now() + 5 * 60 * 1000) // OTP expiry after 5 minutes
+        };
+
+        await userCollection.insertOne(newUser);
+
+        req.session.user = newUser;
+        res.render('user/otp', { title: 'Verify OTP - Elegentpurse' })
+      } else {
+        // Generate 6-digit OTP
+        const otp = Math.floor(100000 + Math.random() * 900000);
+        const hashedPassword = crypash.hash('sha256', req.body.password);
+
+        // Create new user document
+        const newUser = {
+          first_name: req.body.first_name,
+          last_name: req.body.last_name,
+          email: req.body.email,
+          phone: req.body.phone,
+          password: hashedPassword,
+          status: false,
+          otp: otp,
+          otp_expiry: new Date(Date.now() + 5 * 60 * 1000) // OTP expiry after 5 minutes
+        };
+
+        await userCollection.insertOne(newUser);
+
+        req.session.user = newUser;
+        res.render('user/otp', { title: 'Verify OTP - Elegentpurse' })
       }
-
-      if (userExist.otp_expiry > new Date()) {
-        otp = userExist.otp;
-      }
-
-      // Create new user document
-      const newUser = {
-        first_name: req.body.first_name,
-        last_name: req.body.last_name,
-        email: req.body.email,
-        phone: req.body.phone,
-        password: hashedPassword,
-        status: false,
-        otp: otp,
-        otp_expiry: new Date(Date.now() + 5 * 60 * 1000) // OTP expiry after 5 minutes
-      };
-
-      await userCollection.insertOne(newUser);
-
-      req.session.user = newUser;
-      res.render('user/otp', { title: 'Verify OTP - Elegentpurse' })
     } else {
       res.render('user/login', { title: 'Login - Elegentpurse' })
     }
